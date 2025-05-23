ffa971145b2b00cff733051f5de518ba1d19072b
José Guilherme Vanz <jvanz@jvanz.com>
2025-05-23 11:59:45 -0300

    customize astrovim v5
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,9 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+vim.opt.mouse = ""
+vim.opt.colorcolumn = "80,120"
+vim.opt.scrolloff = 10
